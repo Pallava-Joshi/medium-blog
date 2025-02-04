989ddf7fefbd80227bb7fcc8c04b72bfe010a195
Pallava-Joshi <joshipallava@gmail.com>
2025-02-04 15:41:34 +0530

    implemented middleware - next up dividing backend routes into folders

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,15 @@
 import { Hono } from "hono";
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
-import { sign } from "hono/jwt";
+import { sign, verify } from "hono/jwt";
 
 const app = new Hono<{
   Bindings: {
     DATABASE_URL: string;
     JWT_SECRET: string;
+  };
+  Variables: {
+    userId: string;
   };
 }>();
 
@@ -54,18 +57,35 @@ app.post("/api/v1/signin", async (c) => {
   return c.json({ jwt });
 });
 
-app.get("/api/v1/blog/:id", (c) => {
-  const id = c.req.param("id");
-  console.log(id);
-  return c.text("get blog route");
+app.use("/api/v1/blog/*", async (c, next) => {
+  const jwt = c.req.header("Authorization");
+  if (!jwt) return c.json({ error: "unauthorized" });
+
+  const token = jwt.split(" ")[1];
+  const payload = await verify(token, c.env.JWT_SECRET);
+  if (!payload) {
+    c.status(401);
+    return c.json({ error: "unathorised" });
+  }
+  // @ts-ignore
+  c.set("userId", payload.id);
+  // console.log("userId set:", payload.id);
+  await next();
 });
 
 app.post("/api/v1/blog", (c) => {
+  const userId = c.get("userId");
+  console.log("userId retrieved:", userId);
   return c.text("signin route");
 });
 
-app.put("/api/v1/blog", (c) => {
-  return c.text("signin route");
-});
+// app.put("/api/v1/blog", (c) => {
+//   return c.text("signin route");
+// });
 
+// app.get("/api/v1/blog/:id", (c) => {
+//   const id = c.req.param("id");
+//   console.log(id);
+//   return c.text("get blog route");
+// });
 export default app;
